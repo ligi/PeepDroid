6cdfaddb1a7f4ffb9217830039e28fad2db7890e
ligi <ligi@ligi.de>
2018-09-18 20:00:51 +0200

    Cleanup

--- app/src/main/java/org/ligi/peepdroid/InfoActivity.kt
+++ app/src/main/java/org/ligi/peepdroid/InfoActivity.kt
@@ -5,13 +5,9 @@ import android.support.v7.app.AppCompatActivity
 import android.text.method.LinkMovementMethod
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_info.*
-import org.koin.android.ext.android.inject
 import org.ligi.compat.HtmlCompat
-import org.ligi.peepdroid.model.PeepAPI
 
 class InfoActivity : AppCompatActivity() {
-
-    private val peepAPI: PeepAPI by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
